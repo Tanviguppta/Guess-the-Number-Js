850c5c507b0fe081862fe4059904f28d936d1b70
Tanvi Gupta <tanviguppta@gmail.com>
2022-01-03 23:20:31 +0530

    check btn completed

--- script.js
+++ script.js
@@ -4,18 +4,31 @@ document.querySelector('.message').textContent= 'Correct number!';*/
 
 const secretNumber = Math.trunc(Math.random()*20)+ 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number (document.querySelector('.guess').value);
     console.log(guess, typeof guess );
 
     if(score > 1){
+
+        // no input
         if (!guess) {
             document.querySelector('.message').textContent = 'No Number';
-        } else if (guess === secretNumber) {
+        } 
+
+        //correct input  
+        else if (guess === secretNumber) {  
             document.querySelector('.message').textContent= 'Correct Number';
-        } else if(guess > secretNumber) {
+
+            document.querySelector('.number').textContent = secretNumber;
+
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+        } 
+        
+        //wrong input  
+        else if(guess > secretNumber) {
+          
             document.querySelector('.message').textContent = 'Too High !';
             score--;
             document.querySelector('.score').textContent = score;
@@ -25,9 +38,19 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = score;
         }
     }
+
+    //Lost game
     else{
         document.querySelector('.message').textContent= 'You Lost the Game';
         document.querySelector('.score').textContent = 0;
+
+        document.querySelector('body').style.backgroundColor = '#FF0000';
     }
    
-})
+});
+
+document.querySelector('.again').addEventListener('again', function(){
+
+
+    
+});
